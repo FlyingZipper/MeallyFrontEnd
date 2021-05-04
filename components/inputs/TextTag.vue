c7359a97002a40e90094b52b7b4ba2dfcd156626
Philippe Trudel <p.trudel1996@gmail.com>
2021-05-04 15:09:38 -0400

    text tag

---
+++ components/inputs/TextTag.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="w-full">
+    <div class="flex border border-gray-500 rounded-lg">
+      <div class="flex items-center justify-center px-6 bg-gray-400 border-r border-gray-800 rounded-l-lg">
+        <slot />
+      </div>
+      <div class="relative w-full">
+        <input
+          ref="input"
+          :name="name"
+          :type="type"
+          :value="value"
+          class="z-10 w-full p-4 bg-gray-300 rounded-r-lg focus:outline-none"
+          @blur="blur"
+          @focus="focus"
+          @input="$emit('input', $event.target.value)"
+        >
+        <span
+          :class="[
+            minimized
+              ? 'top-0 text-xs text-gray-500'
+              : 'top-1/2 -translate-y-1/2 text-gray-600',
+            'absolute left-0 z-0 mx-4 capitalize duration-150 transform pointer-events-none py-1',
+          ]"
+        >{{ placeholder }}</span>
+      </div>
+    </div>
+    <span v-if="error !== null" class="text-sm text-red-400">{{ error }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      required: false,
+      default: null
+    },
+    placeholder: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    value: {
+      type: undefined,
+      required: false,
+      default: null
+    },
+    focused: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data () {
+    return {
+      minimized: false
+    }
+  },
+  mounted () {
+    if (this.focused) {
+      this.$refs.input.focus()
+    }
+    this.minimizedOnInput()
+  },
+  updated () {
+    this.minimizedOnInput()
+  },
+  methods: {
+    minimizedOnInput () {
+      if (this.$refs.input.value !== null && this.$refs.input.value !== '') {
+        this.minimized = true
+      }
+    },
+    focus () {
+      this.minimized = true
+    },
+    blur () {
+      if (this.value === null || this.value === '') {
+        this.minimized = false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,25 @@
     <h1 class="mb-2 title-1">
       Selling the app - Home
     </h1>
+    <div>
+      <v-text-tag v-model="input" placeholder="ingredient">
+        <span class="text-lg">1</span>
+      </v-text-tag>
+    </div>
   </div>
 </template>
 
 <script>
+import TextTagVue from '~/components/inputs/TextTag.vue'
 export default {
   auth: false,
+  components: {
+    'v-text-tag': TextTagVue
+  },
   data () {
     return {
-      files: []
+      files: [],
+      input: ''
     }
   }
 }
